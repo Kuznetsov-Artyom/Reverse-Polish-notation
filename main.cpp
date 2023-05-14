6c06666144814376bf7ee39f7dfd49ca7a16d060
ArtyomkaKuznetsov <artyomka_working@mail.ru>
2023-05-14 11:55:30 +0300

    refactoring main.cpp

--- main.cpp
+++ main.cpp
@@ -12,8 +12,6 @@
 
 int main()
 {
-	system("chcp 1251 > nul");
-
 	while (true)
 	{
 		std::string str;
@@ -36,49 +34,6 @@ int main()
 			continue;
 		}
 	}
-
-	/*Arithmetic test2("13.3 - 0.3");
-	std::cout << test2.GetSrcStr() << '\n';
-	std::cout << test2.GetPolStr() << '\n';
-	test2.ShowTableVar();
-	std::cout << "Result = " << test2.GetResult() << '\n';*/
-
-
-	Arithmetic test("5 + (5) -7+abc/3.0");
-	std::cout << test.GetSrcStr() << '\n';
-	std::cout << test.GetPolStr() << '\n';
-	test.ShowTableVar();
-	std::cout << "Result = " << test.GetResult() << '\n';
-
-
-	test = "0.5 ^ (0.25 + 0.932 ^ 2.0) / (0.3 + 20) ^ xD";
-	test.SetValVar("xD", 3);
-	test.Calculation();
-	std::cout << test.GetSrcStr() << '\n';
-	std::cout << test.GetPolStr() << '\n';
-	test.ShowTableVar();
-	std::cout << "Result = " << test.GetResult() << '\n';
-
-	std::cout << '\n';
-
-	test = "(4 + 59.6)^(1 / 3) * 0.005 + 3.14^opa";
-	test.SetValVar("opa", 3);
-	test.Calculation();
-	std::cout << test.GetSrcStr() << '\n';
-	std::cout << test.GetPolStr() << '\n';
-	test.ShowTableVar();
-	std::cout << "Result = " << test.GetResult() << '\n';
-
-	std::cout << '\n';
-
-	test = "25^0.5 + (27^(1/3))^2.6";
-	std::cout << test.GetSrcStr() << '\n';
-	std::cout << test.GetPolStr() << '\n';
-	test.ShowTableVar();
-	std::cout << "Result = " << test.GetResult() << '\n';
-
-	
-
 
 	return 0;
 }
